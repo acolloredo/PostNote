2ae27707c1d9486a3bcd35a74f9211660afce929
aidanmaney <86045868+aidanmaney@users.noreply.github.com>
2023-10-19 19:37:28 -0700

    Set project to material3, set Google lato font as default, set placeholder colorscheme.

--- post_note/lib/main.dart
+++ post_note/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:post_note/profile_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,26 +15,48 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-          foregroundColor: Colors.white,
-          toolbarTextStyle: TextStyle(
-            color: Colors.white,
-            backgroundColor: Colors.white,
-          ),
-        ),
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.green,
-      ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: ThemeData(
+          useMaterial3: true,
+          textTheme: GoogleFonts.latoTextTheme(
+            Theme.of(context).textTheme,
+          ),
+          colorScheme: const ColorScheme(
+            primary: Colors.green,
+            brightness: Brightness.light,
+            onPrimary: Colors.white,
+            secondary: Colors.greenAccent,
+            onSecondary: Colors.white,
+            error: Colors.yellow,
+            onError: Colors.red,
+            background: Colors.white,
+            onBackground: Colors.black,
+            surface: Colors.green,
+            onSurface: Colors.white,
+          ),
+          primaryColor: Colors.green,
+          splashFactory: NoSplash.splashFactory,
+          textButtonTheme: TextButtonThemeData(
+            style: TextButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+          ),
+          textSelectionTheme: const TextSelectionThemeData(
+            // selectionColor: Color.fromARGB(255, 144, 202, 249),
+
+            selectionColor: Colors.blue,
+          )
+
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          ),
     );
   }
 }
@@ -79,17 +103,40 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
-        actions: [
-          TextButton(
-            onPressed: () => {},
-            style: TextButton.styleFrom(foregroundColor: Colors.white),
-            child: const Text("Logout"), // TODO: link to logout logic
+        title: SelectionArea(
+          child: Text(widget.title),
+        ),
+        leading: Padding(
+          padding: const EdgeInsets.all(6.0),
+          child: IconButton(
+            icon: const Icon(Icons.home),
+            onPressed: () {},
           ),
-          IconButton(
-            onPressed: () => {},
-            icon: const Icon(Icons.person),
-          ), // TODO: link to profile page route
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(6.0),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.white,
+                backgroundColor: Colors.orange,
+              ),
+              onPressed: () => {}, // TODO: link to logout logic
+              child: const Text("Logout"),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(6.0),
+            child: IconButton(
+              onPressed: () => {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const ProfilePage()))
+              }, // TODO: link to profile page route
+              icon: const Icon(Icons.person),
+            ),
+          ),
         ],
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
@@ -114,9 +161,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+            const Text('You have pushed the button this many times:'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
