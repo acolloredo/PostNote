da444bb3af3d2e656e118db390857b293c5dc531
aidanmaney <86045868+aidanmaney@users.noreply.github.com>
2023-10-19 19:42:56 -0700

    Remove selection area & associated coloring, hardcode app title.

--- post_note/lib/main.dart
+++ post_note/lib/main.dart
@@ -15,54 +15,49 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
       theme: ThemeData(
-          useMaterial3: true,
-          textTheme: GoogleFonts.latoTextTheme(
-            Theme.of(context).textTheme,
+        useMaterial3: true,
+        textTheme: GoogleFonts.latoTextTheme(
+          Theme.of(context).textTheme,
+        ),
+        colorScheme: const ColorScheme(
+          primary: Colors.green,
+          brightness: Brightness.light,
+          onPrimary: Colors.white,
+          secondary: Colors.greenAccent,
+          onSecondary: Colors.white,
+          error: Colors.yellow,
+          onError: Colors.red,
+          background: Colors.white,
+          onBackground: Colors.black,
+          surface: Colors.green,
+          onSurface: Colors.white,
+        ),
+        primaryColor: Colors.green,
+        splashFactory: NoSplash.splashFactory,
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.white,
           ),
-          colorScheme: const ColorScheme(
-            primary: Colors.green,
-            brightness: Brightness.light,
-            onPrimary: Colors.white,
-            secondary: Colors.greenAccent,
-            onSecondary: Colors.white,
-            error: Colors.yellow,
-            onError: Colors.red,
-            background: Colors.white,
-            onBackground: Colors.black,
-            surface: Colors.green,
-            onSurface: Colors.white,
-          ),
-          primaryColor: Colors.green,
-          splashFactory: NoSplash.splashFactory,
-          textButtonTheme: TextButtonThemeData(
-            style: TextButton.styleFrom(
-              foregroundColor: Colors.white,
-            ),
-          ),
-          textSelectionTheme: const TextSelectionThemeData(
-            // selectionColor: Color.fromARGB(255, 144, 202, 249),
+        ),
 
-            selectionColor: Colors.blue,
-          )
-
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
-          ),
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({super.key});
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -73,7 +68,7 @@ class MyHomePage extends StatefulWidget {
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
 
-  final String title;
+  final String title = "PostNote";
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -103,14 +98,19 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        title: SelectionArea(
-          child: Text(widget.title),
-        ),
+        title: Text(widget.title),
         leading: Padding(
           padding: const EdgeInsets.all(6.0),
           child: IconButton(
             icon: const Icon(Icons.home),
-            onPressed: () {},
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const MyHomePage(),
+                ),
+              );
+            },
           ),
         ),
         actions: [
@@ -133,7 +133,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     context,
                     MaterialPageRoute(
                         builder: (context) => const ProfilePage()))
-              }, // TODO: link to profile page route
+              },
               icon: const Icon(Icons.person),
             ),
           ),
