c42961d2e524894a9f18cc844432262e74771d70
aidanmaney <86045868+aidanmaney@users.noreply.github.com>
2023-10-23 11:20:23 -0700

    Update colorscheme and its usage.

---
+++ post_note/lib/palette.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  // static const Color backgroundColor = Color.fromRGBO(215, 196, 152, 1);
+  static const Color mintCream = Color(0xffF1FFFA);
+  static const Color teaGreen = Color(0xffCCFCCB);
+  static const Color outerSpace = Color(0xff464E47);
+  static const Color fernGreen = Color(0xff568259);
+  static const Color celadon = Color(0xff96E6B3);
+  // static const Color greenAccentColor = Color.fromRGBO(37, 66, 36, 1);
+}
