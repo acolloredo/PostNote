bd41e2ec5b61e4144fb80951d3f573aaad779be5
aidanmaney <86045868+aidanmaney@users.noreply.github.com>
2023-10-23 12:14:47 -0700

    Update references (color/packages) in login_screen.dart.

--- post_note/lib/login_screen.dart
+++ post_note/lib/login_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:post_note/Pallete.dart';
+import 'package:post_note/Palette.dart';
 import 'package:post_note/social_button.dart';
 import 'login_field.dart';
 import 'gradient_button.dart';
@@ -12,50 +12,50 @@ class LoginScreen extends StatelessWidget {
     return Scaffold(
       body: SingleChildScrollView(
         child: Center(
-          child: Column(children: [
+          child: Column(children: const [
             Image(
               image: AssetImage('assets/images/Post-Note-Logo.png'),
               width: 300,
               height: 300,
               fit: BoxFit.cover,
             ),
-            const Text(
+            Text(
               'Sign in.',
               style: TextStyle(
                 fontWeight: FontWeight.bold,
-                color: Pallete.black,
+                color: Palette.outerSpace,
                 fontSize: 90,
               ),
             ),
-            const SizedBox(height: 50),
-            const Text(
+            SizedBox(height: 50),
+            Text(
               'A central hub to upload, view, and share your notes!',
               style: TextStyle(
                 fontWeight: FontWeight.bold,
-                color: Pallete.black,
+                color: Palette.outerSpace,
                 fontSize: 25,
                 fontStyle: FontStyle.italic,
               ),
             ),
-            const SizedBox(height: 50),
-            const SocialButton(
+            SizedBox(height: 50),
+            SocialButton(
                 iconPath: 'assets/svgs/g_logo.svg',
                 label: 'Continue with Google'),
-            const SizedBox(height: 15),
-            const Text(
+            SizedBox(height: 15),
+            Text(
               'or',
               style: TextStyle(
                 fontSize: 15,
-                color: Pallete.black,
+                color: Palette.outerSpace,
               ),
             ),
-            const SizedBox(height: 15),
-            const LoginField(hintText: 'Email'),
-            const SizedBox(height: 15),
-            const LoginField(hintText: 'Password'),
-            const SizedBox(height: 20),
-            const GradientButton(),
-            const SizedBox(height: 100)
+            SizedBox(height: 15),
+            LoginField(hintText: 'Email'),
+            SizedBox(height: 15),
+            LoginField(hintText: 'Password'),
+            SizedBox(height: 20),
+            GradientButton(),
+            SizedBox(height: 100)
           ]),
         ),
       ),
