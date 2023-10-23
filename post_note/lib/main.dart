6e545b1b10e5ef1f50fb048096758debc815888c
aidanmaney <86045868+aidanmaney@users.noreply.github.com>
2023-10-23 11:37:43 -0700

    Swap white to mintcream, remove extraneous colors.

--- post_note/lib/main.dart
+++ post_note/lib/main.dart
@@ -38,7 +38,7 @@ class MyApp extends StatelessWidget {
         splashFactory: NoSplash.splashFactory,
         textButtonTheme: TextButtonThemeData(
           style: TextButton.styleFrom(
-            foregroundColor: Colors.white,
+            foregroundColor: Palette.mintCream,
           ),
         ),
       ),
