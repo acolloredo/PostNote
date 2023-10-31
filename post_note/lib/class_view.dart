75abc9a422d31432b382c3a3556ad0725f178c4c
aidanmaney <86045868+aidanmaney@users.noreply.github.com>
2023-10-31 13:47:14 -0700

    Add placeholder fields for Alexander to build from firebase.

--- post_note/lib/class_view.dart
+++ post_note/lib/class_view.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class ClassView extends StatelessWidget {
@@ -6,45 +8,46 @@ class ClassView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(15.0),
-      child: GridView.count(
-        physics: const BouncingScrollPhysics(),
-        crossAxisSpacing: 30.0,
-        mainAxisSpacing: 30.0,
-        childAspectRatio: 1.55,
-        crossAxisCount: 5,
-        children: const [
-          ClassCard(),
-          ClassCard(),
-          ClassCard(),
-          ClassCard(),
-          ClassCard(),
-          ClassCard(),
-          ClassCard(),
-          ClassCard(),
-          ClassCard(),
-          ClassCard(),
-          ClassCard(),
-          ClassCard(),
-          ClassCard(),
-          ClassCard(),
-          ClassCard(),
-          ClassCard(),
-          ClassCard(),
-          ClassCard(),
-          ClassCard(),
-          ClassCard(),
-          ClassCard(),
-          ClassCard(),
-          ClassCard(),
-          ClassCard(),
-          ClassCard(),
-          ClassCard(),
-          ClassCard(),
-          ClassCard(),
-          ClassCard(),
-          ClassCard(),
-        ],
+      padding: const EdgeInsets.all(30.0),
+      child: SingleChildScrollView(
+        child: Center(
+          child: Wrap(
+            // physics: const BouncingScrollPhysics(),
+            // crossAxisSpacing: 30.0,
+            // mainAxisSpacing: 30.0,
+            // childAspectRatio: 1.55,
+            // crossAxisCount: 5,
+
+            // TODO: use a builder when fetching (to not load everything)
+            children: const [
+              ClassCard(),
+              ClassCard(),
+              ClassCard(),
+              ClassCard(),
+              ClassCard(),
+              ClassCard(),
+              ClassCard(),
+              ClassCard(),
+              ClassCard(),
+              ClassCard(),
+              ClassCard(),
+              ClassCard(),
+              ClassCard(),
+              ClassCard(),
+              ClassCard(),
+              ClassCard(),
+              ClassCard(),
+              ClassCard(),
+              ClassCard(),
+              ClassCard(),
+              ClassCard(),
+              ClassCard(),
+              ClassCard(),
+              ClassCard(),
+              ClassCard(),
+            ],
+          ),
+        ),
       ),
     );
   }
@@ -57,13 +60,22 @@ class ClassCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox(
-      // height: double.maxFinite,
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+        minHeight: 200.0,
+        minWidth: 300.0,
+        maxHeight: max(MediaQuery.of(context).size.height / 4, 200),
+        maxWidth: max(MediaQuery.of(context).size.width / 5, 300),
+      ),
       child: Card(
         child: Padding(
-          padding: EdgeInsets.all(5.0),
+          padding: const EdgeInsets.all(5.0),
           child: Card(
             color: Colors.white,
+            child: Column(children: const [
+              Text("Class Number"),
+              Text("Professor Name"),
+            ]),
           ),
         ),
       ),
