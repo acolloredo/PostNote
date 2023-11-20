8be0d497eb98f0275792cce8618afd8607d11b98
rsol1225 <romasolapurkar14@gmail.com>
2023-11-19 22:40:05 -0800

    fixed overflow

--- post_note/lib/WeekFolder.dart
+++ post_note/lib/WeekFolder.dart
@@ -4,51 +4,57 @@ import 'package:post_note/palette.dart';
 class WeekFolder extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
     return Material(
-      child: Stack(
-        children: <Widget>[
-          ClipRRect(
-              borderRadius: BorderRadius.circular(30.0),
-              child: Center(
-                child: Align(
-                  alignment: Alignment.topCenter,
-                  child: Container(
-                    alignment: Alignment.center,
-                    width: 200,
-                    height: 75,
-                    margin: EdgeInsets.all(15.0),
-                    decoration: BoxDecoration(
-                      color: Palette.outerSpace,
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    child: Text(
-                      "Week X",
-                      style: const TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 40),
-                    ),
-                  ),
-                ),
-              )),
-          ClipRRect(
-              borderRadius: BorderRadius.circular(30.0),
-              child: Center(
-                child: Align(
-                  alignment: Alignment.center,
-                  child: Container(
-                    alignment: Alignment.center,
-                    width: 1000,
-                    height: 500,
-                    margin: EdgeInsets.all(15.0),
-                    decoration: BoxDecoration(
-                      color: Palette.outerSpace,
-                      borderRadius: BorderRadius.circular(10.0),
+      child: SingleChildScrollView(
+        child: Wrap(
+          spacing: 8.0,
+          runSpacing: 4.0,
+          children: <Widget>[
+            ClipRRect(
+                borderRadius: BorderRadius.circular(30.0),
+                child: Center(
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Container(
+                      alignment: Alignment.center,
+                      width: 200,
+                      height: 75,
+                      margin: EdgeInsets.all(15.0),
+                      decoration: BoxDecoration(
+                        color: Palette.outerSpace,
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Text(
+                        "Week X",
+                        style: const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 40),
+                      ),
                     ),
                   ),
-                ),
-              )),
-        ],
+                )),
+            ClipRRect(
+                borderRadius: BorderRadius.circular(30.0),
+                child: Center(
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Container(
+                      alignment: Alignment.bottomCenter,
+                      width: width,
+                      height: height,
+                      margin: EdgeInsets.all(15.0),
+                      decoration: BoxDecoration(
+                        color: Palette.outerSpace,
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                  ),
+                )),
+          ],
+        ),
       ),
     );
   }
