535476ed5d3b0422364a27678191da76ce4f299b
rsol1225 <87049514+rsol1225@users.noreply.github.com>
2023-11-28 11:52:22 -0800

    adding the weekfolder with upload page connection (#32)

--- post_note/lib/WeekFolder.dart
+++ post_note/lib/WeekFolder.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:post_note/palette.dart';
+import 'package:post_note/upload_page.dart';
 
 class WeekFolder extends StatelessWidget {
   final int weekNumber;
@@ -13,53 +14,75 @@ class WeekFolder extends StatelessWidget {
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
-    return Material(
-      child: SingleChildScrollView(
-        child: Wrap(
-          spacing: 8.0,
-          runSpacing: 4.0,
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => UploadPage(
+                className: "YourClassName",
+                weekNumber: weekNumber,
+              ),
+            ),
+          );
+        },
+        child: Text('Upload'),
+        backgroundColor: Palette.fernGreen,
+      ),
+      persistentFooterButtons: <Widget>[
+        // Place any additional persistent footer buttons here if needed
+      ],
+      body: SingleChildScrollView(
+        child: Stack(
           children: <Widget>[
+            // Bottom ClipRRect
             ClipRRect(
-                borderRadius: BorderRadius.circular(30.0),
-                child: Center(
-                  child: Align(
-                    alignment: Alignment.topCenter,
-                    child: Container(
-                      alignment: Alignment.center,
-                      width: 200,
-                      height: 75,
-                      margin: EdgeInsets.all(15.0),
-                      decoration: BoxDecoration(
-                        color: Palette.outerSpace,
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      child: Text(
-                        'Week ${weekNumber}',
-                        style: const TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 40),
-                      ),
+              borderRadius: BorderRadius.circular(30.0),
+              child: Center(
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Container(
+                    alignment: Alignment.bottomCenter,
+                    width: width,
+                    height: height,
+                    margin: EdgeInsets.all(15.0),
+                    decoration: BoxDecoration(
+                      color: Palette.outerSpace,
+                      borderRadius: BorderRadius.circular(10.0),
                     ),
                   ),
-                )),
+                ),
+              ),
+            ),
+
+            // Top ClipRRect
             ClipRRect(
-                borderRadius: BorderRadius.circular(30.0),
-                child: Center(
-                  child: Align(
+              borderRadius: BorderRadius.circular(30.0),
+              child: Center(
+                child: Align(
+                  alignment: Alignment.topCenter,
+                  child: Container(
                     alignment: Alignment.center,
-                    child: Container(
-                      alignment: Alignment.bottomCenter,
-                      width: width,
-                      height: height,
-                      margin: EdgeInsets.all(15.0),
-                      decoration: BoxDecoration(
-                        color: Palette.outerSpace,
-                        borderRadius: BorderRadius.circular(10.0),
+                    width: 200,
+                    height: 75,
+                    margin: EdgeInsets.all(15.0),
+                    decoration: BoxDecoration(
+                      color: Palette.outerSpace,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Text(
+                      'Week ${weekNumber}',
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 40,
                       ),
                     ),
                   ),
-                )),
+                ),
+              ),
+            ),
           ],
         ),
       ),
