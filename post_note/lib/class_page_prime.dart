972d8d890812f022c61967415761f05b75242850
Alexander Colloredo-Mansfeld <acollore@ucsc.edu>
2023-12-03 18:38:18 -0800

    pre shifting weeks page to top of page

--- post_note/lib/class_page_prime.dart
+++ post_note/lib/class_page_prime.dart
@@ -38,38 +38,40 @@ class ClassPagePrime extends StatelessWidget {
           ),
         ),
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(8.0),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
         child: Center(
           child: Row(
             children: <Widget>[
-              Expanded(
+              const Expanded(
                 flex: 1,
-                child: Flex(
-                  direction: Axis.vertical,
+                child: Column(
                   children: [
                     Expanded(
                       flex: 1,
-                      child: SizedBox(
-                        width: double.infinity,
-                        child: Card(
-                          color: Palette.outerSpace,
-                          child: Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: Card(
-                              color: Palette.teaGreen,
-                              child: Column(
-                                children: [
-                                  Padding(
-                                    padding: EdgeInsets.fromLTRB(
-                                        8.0, 16.0, 8.0, 16.0),
-                                    child: Text(
-                                      "Class Study Groups",
-                                      style: TextStyle(fontSize: 30),
-                                      textAlign: TextAlign.center,
+                      child: Padding(
+                        padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 4.0),
+                        child: SizedBox(
+                          width: double.infinity,
+                          child: Card(
+                            color: Palette.outerSpace,
+                            child: Padding(
+                              padding: EdgeInsets.all(4.0),
+                              child: Card(
+                                color: Palette.teaGreen,
+                                child: Column(
+                                  children: [
+                                    Padding(
+                                      padding: EdgeInsets.fromLTRB(
+                                          8.0, 16.0, 8.0, 16.0),
+                                      child: Text(
+                                        "Class Study Groups",
+                                        style: TextStyle(fontSize: 30),
+                                        textAlign: TextAlign.center,
+                                      ),
                                     ),
-                                  ),
-                                ],
+                                  ],
+                                ),
                               ),
                             ),
                           ),
@@ -78,26 +80,29 @@ class ClassPagePrime extends StatelessWidget {
                     ),
                     Expanded(
                       flex: 1,
-                      child: SizedBox(
-                        width: double.infinity,
-                        child: Card(
-                          color: Palette.outerSpace,
-                          child: Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: Card(
-                              color: Palette.teaGreen,
-                              child: Column(
-                                children: [
-                                  Padding(
-                                    padding: EdgeInsets.fromLTRB(
-                                        8.0, 16.0, 8.0, 16.0),
-                                    child: Text(
-                                      "My Study Groups",
-                                      style: TextStyle(fontSize: 30),
-                                      textAlign: TextAlign.center,
+                      child: Padding(
+                        padding: EdgeInsets.fromLTRB(0.0, 4.0, 0.0, 0.0),
+                        child: SizedBox(
+                          width: double.infinity,
+                          child: Card(
+                            color: Palette.outerSpace,
+                            child: Padding(
+                              padding: EdgeInsets.all(4.0),
+                              child: Card(
+                                color: Palette.teaGreen,
+                                child: Column(
+                                  children: [
+                                    Padding(
+                                      padding: EdgeInsets.fromLTRB(
+                                          8.0, 16.0, 8.0, 16.0),
+                                      child: Text(
+                                        "My Study Groups",
+                                        style: TextStyle(fontSize: 30),
+                                        textAlign: TextAlign.center,
+                                      ),
                                     ),
-                                  ),
-                                ],
+                                  ],
+                                ),
                               ),
                             ),
                           ),
@@ -109,23 +114,59 @@ class ClassPagePrime extends StatelessWidget {
               ),
               Expanded(
                   flex: 2,
-                  child: SizedBox(
-                    height: double.infinity,
-                    child: Card(
-                      color: Palette.outerSpace,
-                      child: Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Card(
-                          color: Palette.teaGreen,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                "TEST",
-                                style: TextStyle(fontSize: 30.0),
-                              ),
-                              Text("TEST", style: TextStyle(fontSize: 24.0)),
-                            ],
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(8.0, 0.0, 0.0, 0.0),
+                    child: SizedBox(
+                      height: double.infinity,
+                      child: Card(
+                        color: Palette.outerSpace,
+                        child: Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: Card(
+                            color: Palette.teaGreen,
+                            child: Column(
+                              children: [
+                                const Padding(
+                                  padding:
+                                      EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 16.0),
+                                  child: Text(
+                                    "Weeks",
+                                    style: TextStyle(fontSize: 40.0),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Padding(
+                                    padding: const EdgeInsets.fromLTRB(
+                                        16.0, 32.0, 16.0, 16.0),
+                                    child: SizedBox(
+                                      child: LayoutBuilder(
+                                          builder: (context, constraints) {
+                                        return GridView.builder(
+                                            gridDelegate:
+                                                SliverGridDelegateWithFixedCrossAxisCount(
+                                              crossAxisCount: 5,
+                                              mainAxisSpacing: 16.0,
+                                              mainAxisExtent:
+                                                  constraints.maxHeight / 4,
+                                            ),
+                                            itemCount: 10,
+                                            itemBuilder: (BuildContext context,
+                                                int index) {
+                                              return Card(
+                                                color: Palette.fernGreen,
+                                                child: Text(
+                                                  "Week $index Notes",
+                                                  style: const TextStyle(
+                                                      fontSize: 20),
+                                                ),
+                                              );
+                                            });
+                                      }),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
                           ),
                         ),
                       ),
